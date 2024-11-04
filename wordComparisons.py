3d92e289d1719511c976a3390f33182a9e137d4c
William2716057 <77903649+William2716057@users.noreply.github.com>
2024-11-05 08:46:11 +1300

    updated

--- wordComparisons.py
+++ wordComparisons.py
@@ -3,14 +3,20 @@ import spacy
 nlp = spacy.load("en_core_web_lg")
 
 word1 = "red"
-word2 = "pink"
+word2 = "ref"
 
 word1 = nlp.vocab[word1]
 word2 = nlp.vocab[word2]
 
 print("Word similarity: ", word1.similarity(word2))
 
-sentence1 = nlp("this is good")
-sentence2 = nlp("This is bad")
+sentence1 = input(nlp("Enter sentence 1: "))
+sentence2 = input(nlp("Enter sentence 2: "))
+
+sentence1 = nlp(sentence1)
+sentence2 = nlp(sentence2)
 
 print("Sentence similarity: ", sentence1.similarity(sentence2))
+
+#sentence1_verbs = 
+print(" ".join([token.lemma_ for token in sentence1 if token.pos_ == "VERB"]))
