eb01dc9f3373b15a8d9f12a14cf05da5754442de
William2716057 <77903649+William2716057@users.noreply.github.com>
2024-11-05 08:27:33 +1300

    sentences added

--- wordComparisons.py
+++ wordComparisons.py
@@ -8,5 +8,9 @@ word2 = "pink"
 word1 = nlp.vocab[word1]
 word2 = nlp.vocab[word2]
 
-print(word1.similarity(word2))
+print("Word similarity: ", word1.similarity(word2))
 
+sentence1 = nlp("this is good")
+sentence2 = nlp("This is bad")
+
+print("Sentence similarity: ", sentence1.similarity(sentence2))
